a24d882766d71869903b6e9e0a71cf6e8d38d044
Andreas Larsson <andreas.larsson@leovegas.com>
2018-12-03 14:11:05 +0100

    Added shell-script to start a new day with

---
+++ createDay.sh
@@ -0,0 +1,12 @@
+if [ ! -d "$1" ]; then
+  echo "Creating directory for day: $1"
+  mkdir $1
+  echo "Touching input-files"
+  touch $1/input.txt
+  touch $1/test.txt
+  echo "Copying template"
+  cp template.py $1/solve.py
+  echo "Done!"
+else
+  echo "Day $1 already exists, aborting!"
+fi
